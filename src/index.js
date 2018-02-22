bf21a79cf549d08253c58cb8114feb4ac3b88b6d
arihito <arihito.m@gmail.com>
2018-02-22 20:38:09 +0900

    fix ava testcase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const test = require('ava')
 
 function sum (...nums) {
-  return nums.reduce((a, b) => b)
+  return nums.reduce((a, b) => a + b)
 }
 
-test('sum of 1, 2, 3, 4 must be 10', () => {
-  sum(1, 2, 3, 4)
+test('sum of 1, 2, 3, 4 must be 10', (t) => {
+  t.is(sum(1, 2, 3, 4), 10)
 })
